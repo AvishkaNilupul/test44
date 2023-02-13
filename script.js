8c006008c1efd198b81524834d0e00cb098bc894
AvishkaNilupul <121239381+AvishkaNilupul@users.noreply.github.com>
2023-02-13 20:27:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 
 function change() {
     var image = document.getElementById('img11');
-    image.src = '/images/girl11.webp';
+    image.src = 'images/girl11.webp';
 
 }
 function change23(){
     var image = document.getElementById('img11');
-    image.src = '/images/girl1.webp';
+    image.src = 'images/girl1.webp';
 
 }
 
 function change2(){
     var image = document.getElementById('imgb22');
-    image.src = '/images/b22.webp';
+    image.src = 'images/b22.webp';
 }
 function change22(){
     var image = document.getElementById('imgb22');
-    image.src = '/images/16750602692df9c489a41b446301f282ab9a00f260_thumbnail_600x.webp';
+    image.src = 'images/16750602692df9c489a41b446301f282ab9a00f260_thumbnail_600x.webp';
 }
 
 function changegirl1(){
     var image = document.getElementById('girl33');
-    image.src = '/images/girl3.webp';
+    image.src = 'images/girl3.webp';
 
 }
 function changegirl2(){
